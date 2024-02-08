0ab2078fa319113bc70164b6324330c704781124
SupplyingCoffins <mikehart260@gmail.com>
2024-02-07 22:59:46 -0800

    updates print statement

--- homework/HW1/main.py
+++ homework/HW1/main.py
@@ -5,26 +5,26 @@
 from numpy import loadtxt as ld
 def get_file():
     return input("filename: ")
+
 def get_elevation():
     return input("Elevation: ")
+
 def validate_file(file):
     try: data = ld(file,skiprows=1,dtype=str,delimiter=',')
     except: print("File not found.")
     else: return True
     return False
+
 def validate_elevation(elevation):
     try: float(elevation)
     except ValueError: print("Elevation should be float.")
     else: return True
     return False
-def find_distance(file, desired_elevation):
 
+def find_distance(file, desired_elevation):
     data = ld(file,skiprows=1,dtype=str,delimiter=',')
     diff = 1000
     idx = 0
-    # distance = line[0]
-    # elevation = line[1]
-    # idx = data[line]
     for line in data:
         if abs(desired_elevation - float(line[1])) < diff:
             diff = abs(desired_elevation - float(line[1]))
@@ -42,4 +42,4 @@ while (not validate_elevation(desired_elevation)):
     desired_elevation = get_elevation()
 
 distance , elevation = find_distance(file,float(desired_elevation))
-print("Distance : {0:.3f} m, Elevation : {1:.3f} m".format(distance, elevation))
+print("Distance : {0:.3f} m".format(distance))
