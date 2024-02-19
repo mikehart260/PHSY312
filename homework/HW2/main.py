879c206d5f07dcf8663173154cd6267a8196f2f4
Michael <mikehart260@gmail.com>
2024-02-19 12:53:18 -0800

    Load's data and plots. TODO: normalize and convert wavelength to nm

--- homework/HW2/main.py
+++ homework/HW2/main.py
@@ -3,3 +3,30 @@
 Michael Hart
 Main file for HW2.
 '''
+# imports
+import numpy as np
+import os
+import matplotlib.pyplot as plt
+
+# Load in files that begin with uk
+directory = os.listdir('StellarSpectraFiles')
+for filename in directory:
+    wavelength = []
+    intensity = []
+    if filename[:2] == "uk":
+        full_filename = "StellarSpectraFiles/" + filename
+        filename = filename.split('.')
+        data = np.loadtxt(full_filename,skiprows=3,usecols=[0,1])
+        for line in data:
+            if 6650 >= int(line[0]) >= 6500:
+                wavelength.append(int(line[0]))
+                intensity.append(float(line[1]))
+        plt.plot(wavelength,intensity,label=filename[0][2:])         
+
+plt.xlabel("Wavelength")
+plt.ylabel("Intesity")
+plt.legend()
+plt.show()
+
+
+
