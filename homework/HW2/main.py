babca7ce2a760a85a8d01714175379b9bcdb94fb
Michael <mikehart260@gmail.com>
2024-02-21 10:57:07 -0800

    working version ready to submit, WIP: sorting legend

--- homework/HW2/main.py
+++ homework/HW2/main.py
@@ -7,15 +7,17 @@ Main file for HW2.
 import numpy as np
 import os
 import matplotlib.pyplot as plt
-
+fig = plt.figure()
 # Load in files that begin with uk
 directory = os.listdir('StellarSpectraFiles')
+star_types = []
 for filename in directory:
     wavelength = []
     intensity = []
     if filename[:2] == "uk":
         full_filename = "StellarSpectraFiles" +os.sep + filename
         filename = filename.split('.')
+        star_types.append(f"Type {(filename[0][2]).upper()}")
         data = np.loadtxt(full_filename,skiprows=3,usecols=[0,1])
         sum = 0
         count = 0
@@ -27,7 +29,28 @@ for filename in directory:
             if 6500 <= int(line[0]) <= 6650: 
                 wavelength.append(int(line[0])*0.1) # convert to nm
                 intensity.append(float(line[1]))
-        plt.plot(wavelength,np.array(intensity)/(sum/count),label=f"Type {filename[0][2]}")
+        plt.plot(wavelength,np.array(intensity)/(sum/count),label=f"Type {(filename[0][2]).upper()}")
+
+# Stack overflow code https://stackoverflow.com/questions/50791422/plotting-sequences-with-same-label-and-color-in-python
+ax = fig.gca() # get current axis
+for i, p in enumerate(ax.get_lines()):
+    if p.get_label() in star_types[:i]:
+        idx = star_types.index(p.get_label())       # find ist index
+        p.set_c(ax.get_lines()[idx].get_c())   # set color
+        p.set_label('_' + p.get_label()) 
+
+# my code to potentially sort the legend. 
+# lines = []
+# for i, p in enumerate(ax.get_lines()):
+#     print("i: ",i," p: ",p)
+#     if len(lines) == 0:
+#         lines.append(ax.get_lines()[i])
+#     else:
+#         for line in lines:
+#             if p.get_label()[-1] < line.get_label()[-1]:
+#                 idx = lines.index(line)
+#                 lines.insert(idx,ax.get_lines()[i])
+# print(lines)
 
 plt.xlabel("Wavelength (nm)")
 plt.ylabel("Intesity")
