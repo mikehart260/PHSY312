16d632390f5eb4f9ba5fccfce976e83ea90baf5f
Michael <mikehart260@gmail.com>
2024-02-29 14:42:31 -0800

    HW3: progress

--- homework/HW3/avg_distance.py
+++ homework/HW3/avg_distance.py
@@ -11,11 +11,14 @@ data = np.loadtxt("Hipparcos_Plx_20_25.dat",skiprows=1)
 # DE = data[:,3]
 
 '''
-For stars nearby the average positon, find their average distance from it. 
+For stars nearby the average positon, find their average distance from it.
+This is valid because this area of stars is more dense than normal.
 '''
 plt.close('all')
 RA = []
 DE = []
+avg_RA_list = []
+avg_DE_list = []
 std_hist = []
 avg_hist = []
 for i in range(200): 
@@ -26,7 +29,7 @@ for i in range(200):
     RA = []
     DE = []
     for line in data:
-        if (50.0+rand <= line[2] <= 82.0+rand and 6.0+rand <= line[3] <= 26.0+rand):
+        if (50.0+2*rand <= line[2] <= 82.0+2*rand and 6.0+2*rand <= line[3] <= 26.0+2*rand):
             RA.append(line[2])
             DE.append(line[3])
             avg_RA += line[2]
@@ -34,20 +37,29 @@ for i in range(200):
             count+= 1
     avg_RA /= count
     avg_DE /= count
-
+    
     distances = []
     for i in range(len(RA)):
         x = RA[i]
         y = DE[i]
         distances.append( np.sqrt((avg_RA-x)**2 + (avg_DE - y)**2) )
     distances = np.array(distances) 
-
+    avg_DE_list.append(avg_DE)
+    avg_RA_list.append(avg_RA)
     avg_distance_from_center = np.average(distances)
     std_distances = np.std(distances)
     #print(f"Average Distance: {avg_distance_from_center}, STDEV: {std_distances}")
     std_hist.append(std_distances)
     avg_hist.append(avg_distance_from_center)
 
+print(f"Average DE: {np.average(avg_DE_list)}, Std deviation of average: {np.std(avg_DE_list)}")
+print(f"Average RA: {np.average(avg_RA_list)}, Std deviation of average: {np.std(avg_RA_list)}")
+
+'''
+RESULTS: 
+    Average DE: 16.5 Std dev 0.705
+    Average RA: 66.1 Std dev 0.70
+'''
 plt.hist(avg_hist)
 plt.title("Average distance")
 plt.show()
--- homework/HW3/velocities.py
+++ homework/HW3/velocities.py
@@ -12,7 +12,6 @@ data = np.loadtxt("Hipparcos_Plx_20_25.dat",skiprows=1)
 Here we calculated the most common direction from this patch of sky to be -80 degress.
 '''
 
-
 def vector_dir( v_RA , v_DE):
      theta = np.arctan2(v_DE,v_RA)
      return np.degrees(theta)
@@ -22,47 +21,61 @@ def mode(arr):
     max_count_index = np.argmax(counts)
     return unique[max_count_index]
 
-# directions = []
-
-# for line in data:
-#         if (50.0 <= line[2] <= 82.0 and 6.0 <= line[3] <= 26.0):
-#             dir = vector_dir(1/line[5],1/line[6])
-#             directions.append(dir) 
-            
-# plt.arrow(line[2],line[3],1/line[5],1/line[6],width=1,head_width = 1)
-                
-# directions = np.array(directions)
-
-# print(mode(np.round(directions))) # MODE was 80
-
 '''
-    Now lets calculate the residuals of the stars directions. 
-'''
-DRIFT = -80
-dirs = []
+    CODE TO FIND THE MOST COMMON VELOCITY OF THE CLUSTER.
+
+directions = []
+
 for line in data:
-    if (50.0 <= line[2] <= 82.0 and 6.0 <= line[3] <= 26.0):
+         if (50.0 <= line[2] <= 82.0 and 6.0 <= line[3] <= 26.0):
+             dir = vector_dir(1/line[5],1/line[6])
+             directions.append(dir) 
+            
+plt.arrow(line[2],line[3],1/line[5],1/line[6],width=1,head_width = 1)
+                
+directions = np.array(directions)
+
+print(mode(np.round(directions))) # MODE was 80
+'''
+
+'''
+    PLOTTING STARS IN THE CLUSTER TO INCLUDE VS NOT INCLUDE.
+    ALSO CALCULATE AVERAGE AND STDEV OF THE VELOCITIES.
+
+    RESULTS: 
+'''
+
+dirs = []
+count = 0
+for line in data:
+    RA = line[2]
+    DEC = line[3]
+    if(46.1 <= RA <= 86.1 and 0.0 <= DEC <= 32.0):
         dir = vector_dir(1/line[5],1/line[6])
-        if (-95 < dir < -67):
+        if (-90.207 < dir < -61.803): # Two standard deviations
             print(dir)
-            plt.arrow(line[2],line[3],1/line[5],1/line[6],width=1,head_width = 1)
+            plt.arrow(RA,DEC,1/line[5],1/line[6],width=1,head_width = 1)
             dirs.append(dir)
+        else:
+            plt.arrow(RA,DEC,1/line[5],1/line[6],width=1,head_width = 1, color='red')            
 
 
-print(len(dirs))
+print(f"Number of stars included: {len(dirs)}")
 print(np.average(dirs))
 print(np.std(dirs))
 
-''' -71.2626 -> -80.7486 is one stdev'''
+''' -71.2626 -> -80.7486 is one stdev
+    -66.537  -> -85.473 is two stdev
+'''
 
 
 plt.xlabel("East-West")
 plt.ylabel("North-South")
-plt.title("Most common direction is -80 degrees.")
+plt.title("Cluster")
 plt.show()
 
 plt.figure()
 plt.hist(dirs)
-plt.legend()
+
 plt.show()
 
