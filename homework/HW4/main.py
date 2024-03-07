5eeac309e06fb8f825b37933446ed6093042074d
Michael <mikehart260@gmail.com>
2024-03-07 13:50:40 -0800

    HW4: Some progress.

--- homework/HW4/main.py
+++ homework/HW4/main.py
@@ -9,7 +9,7 @@ def myFunc(n, masses, springs):
     springs: Array containing spring constants, with a size n+1
     
     Returns:
-    array: eigenvalues
+    array: eigenvalues, eigenvectors
     '''
     K = np.zeros((n,n)) # initialize matrix
 
@@ -28,14 +28,17 @@ def myFunc(n, masses, springs):
     
     # get frequencies
     frequencies = np.sqrt(values/masses)
-    return frequencies
+    return frequencies, vectors
 
 '''
-PLOT 1: Normal Mode Frequencies vs Number of Mases with all spring constants of 1 N/m and masses of 1kg
+PLOT 1: Normal Mode Frequencies vs Number of Mases with all spring constants
+of 1 N/m and masses of 1kg
 '''
+
 plt.figure(1)
 for n in range(2,31):
-    for w in myFunc(n,np.ones(n),np.ones(n+1)):
+    freq,v = myFunc(n,np.ones(n),np.ones(n+1))
+    for w in freq:
         plt.scatter(n,w)
 
 plt.xticks([5,10,15,20,25,30])
@@ -45,14 +48,29 @@ plt.title('all k\'s = 1 N/m all masses = 1 kg')
 plt.close(1)
 
 '''
-PLOT 2: 
+PLOT 2: Normal Mode Number
 '''
+
 plt.figure(2)
 n = 30
-frequencies = myFunc(n,np.ones(n),np.ones(n+1))
-for i,f in enumerate(frequencies):
-    plt.scatter(i,f)
-
-plt.show()
+frequencies, vec = myFunc(n,np.ones(n),np.ones(n+1))
+plt.scatter(range(0,len(frequencies)), np.sort(frequencies))
+plt.xlabel("Normal Mode Number")
+plt.ylabel("Normal Mode Frequency (rad/s)")
+# plt.show()
 plt.close(2)
 
+'''
+PLOT 3: 
+'''
+plt.figure(3)
+print(frequencies)
+
+max_freq_idx = np.argmax(frequencies)
+min_freq_idx = np.argmin(frequencies)
+
+for mass_idx in range(0,n):
+    plt.scatter(mass_idx, vec[max_freq_idx][mass_idx], color='g', marker='.')
+    plt.scatter(mass_idx, vec[min_freq_idx][mass_idx], color='blue', marker='s')
+
+plt.show()
